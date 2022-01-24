e7a4ebace954c0cad3f75462c1d70addd30eb265
Aaron <kilpatrick.aaron92@gmail.com>
2022-01-24 21:05:07 +1100

    Added comment

--- mailbox_remaker.py
+++ mailbox_remaker.py
@@ -9,6 +9,7 @@ for root, dirs, files in os.walk(filepath, topdown=True):
         folder_name = dir.split('.', 1)[0]
         original_path = os.path.join(f"{root}{dir}", 'mbox')
 
+        # Copy and rename file into given directory
         try:
             shutil.copyfile(original_path, f'{newfolder}{folder_name}')
         except shutil.Error:
